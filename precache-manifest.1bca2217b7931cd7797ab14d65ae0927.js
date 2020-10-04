b8247fdd5d8b03ecb003b342c667687016be9166
fabemiliano <fab.emiliano@gmail.com>
2020-10-03 21:03:10 -0300

    Updates

---
+++ precache-manifest.1bca2217b7931cd7797ab14d65ae0927.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e2e7864cb52edd10721bd769b783512",
+    "url": "/movieApp/index.html"
+  },
+  {
+    "revision": "1166f0a90027ba0346e7",
+    "url": "/movieApp/static/css/main.5415138d.chunk.css"
+  },
+  {
+    "revision": "ef13b3e8aa122c210a60",
+    "url": "/movieApp/static/js/2.94fb8f33.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/movieApp/static/js/2.94fb8f33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1166f0a90027ba0346e7",
+    "url": "/movieApp/static/js/main.b6dc3204.chunk.js"
+  },
+  {
+    "revision": "9683273862b3a76620a2",
+    "url": "/movieApp/static/js/runtime-main.42d387db.js"
+  },
+  {
+    "revision": "fca62fb29c4c383b3f94a970b26b6160",
+    "url": "/movieApp/static/media/loader.fca62fb2.png"
+  },
+  {
+    "revision": "f1f7de47229dddd554b55b214b881675",
+    "url": "/movieApp/static/media/logo.f1f7de47.png"
+  },
+  {
+    "revision": "c5f8473fe4970a647244b55709074b30",
+    "url": "/movieApp/static/media/loupe.c5f8473f.svg"
+  }
+]);
